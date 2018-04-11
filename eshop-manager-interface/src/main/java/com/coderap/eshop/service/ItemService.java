3ba37c6213a711e74b8868e1771df4acbe20f08a
LennonChin <i@coderap.com>
2018-04-12 00:26:06 +0800

    1, some modifications;

---
+++ eshop-manager-interface/src/main/java/com/coderap/eshop/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.coderap.eshop.service;
+
+import com.coderap.eshop.pojo.TbItem;
+
+/**
+ * @program: eshop-parent
+ * @description: 商品管理Service
+ * @author: Lennon Chin
+ * @create: 2018/04/10 23:28:55
+ */
+public interface ItemService {
+    TbItem getItemById(long itemId);
+}
